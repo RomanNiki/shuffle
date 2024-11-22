781bbab222ab0b78e761030d35cd8e2da559cc6b
Roman Nikiforov <rnikiforov@openmygame.com>
2024-11-22 11:43:42 +0300

    refactoring

--- Assets/App/Scripts/Generator/Handlers/HandlerSorterGroups.cs
+++ Assets/App/Scripts/Generator/Handlers/HandlerSorterGroups.cs
@@ -11,6 +11,7 @@ namespace App.Scripts.Generator.Handlers
         private readonly ConfigSorterGroups _configSorterGroups;
         private readonly IProviderRandom _providerRandom;
         private int _targetGroupsCount;
+        private int _targetUseItems;
 
         public HandlerSorterGroups(ConfigSorterGroups configSorterGroups, IProviderRandom providerRandom)
         {
@@ -18,7 +19,19 @@ namespace App.Scripts.Generator.Handlers
             _providerRandom = providerRandom;
         }
 
-        // var index = i * matrixSize + j;
+        List<Color> GenerateColors(int count)
+        {
+            HashSet<Color> uniqueColors = new HashSet<Color>();
+
+            while (uniqueColors.Count < count)
+            {
+                Color color = new Color(_providerRandom.Value, _providerRandom.Value, _providerRandom.Value);
+                uniqueColors.Add(color);
+            }
+
+            return new List<Color>(uniqueColors);
+        }
+
 
         int GetIndex(int i, int j, int size)
         {
@@ -31,18 +44,19 @@ namespace App.Scripts.Generator.Handlers
             var matrixSize = (int)Mathf.Sqrt(items.Count);
             var percent = _configSorterGroups.percent;
             _targetGroupsCount = (int)(items.Count * percent / targetGroupSize);
-            var targetUseItems = _targetGroupsCount * targetGroupSize;
+            _targetUseItems = _targetGroupsCount * targetGroupSize;
 
             List<GridItem> freeItems = new List<GridItem>();
             List<GridItem> usedItems = new List<GridItem>();
+            List<Color> generateColors = GenerateColors(_targetGroupsCount + 1);
             freeItems.AddRange(items);
 
-            int islandCount = 0;
             int err = 0;
             List<GridItem> island = new List<GridItem>();
-            while (usedItems.Count < targetUseItems && freeItems.Count > targetGroupSize)
+            int islandCount = 0;
+            while (usedItems.Count < _targetUseItems && freeItems.Count >= targetGroupSize)
             {
-                if (err >= targetUseItems * targetUseItems + targetUseItems)
+                if (err >= _targetUseItems * _targetUseItems + _targetUseItems)
                 {
                     throw new StackOverflowException($"at index {island[0].gridPosition}");
                 }
@@ -50,6 +64,7 @@ namespace App.Scripts.Generator.Handlers
                 err++;
                 island.Clear();
                 freeItems.Shuffle();
+
                 var currentItem = freeItems[0];
 
                 if (!TryCreateIsland(currentItem, targetGroupSize, items, usedItems, island))
@@ -57,9 +72,9 @@ namespace App.Scripts.Generator.Handlers
                     continue;
                 }
 
-                var color = new Color(_providerRandom.Value, _providerRandom.Value, _providerRandom.Value, 1);
-
+                var color = generateColors[islandCount];
                 islandCount++;
+                
                 foreach (var islandItem in island)
                 {
                     usedItems.Add(islandItem);
@@ -85,7 +100,7 @@ namespace App.Scripts.Generator.Handlers
                 var item = stack.Pop();
                 Vector2Int itemPosition = item.gridPosition;
 
-                _directions.Shuffle();
+                //_directions.Shuffle();
 
                 foreach (var direction in _directions)
                 {
@@ -117,40 +132,41 @@ namespace App.Scripts.Generator.Handlers
             return island.Count == targetGroupSize;
         }
 
-        private bool WillCreateEmptySpace(GridItem neighborItem, List<GridItem> gridItems, List<GridItem> usedItems,
+        private bool WillCreateEmptySpace(GridItem currentItem, List<GridItem> gridItems, List<GridItem> usedItems,
             HashSet<GridItem> visited, int targetGroupSize)
         {
-            HashSet<GridItem> visitedLocal = new HashSet<GridItem> { neighborItem };
+            HashSet<GridItem> visitedLocal = new HashSet<GridItem> { currentItem };
             visitedLocal.AddRange(visited);
-            int c = 0;
+            int approvedDirections = 0;
             foreach (var direction in _directions)
             {
-                var neighborPosition = neighborItem.gridPosition + direction;
+                var neighborPosition = currentItem.gridPosition + direction;
 
                 if (IsWithinBounds(neighborPosition, (int)Mathf.Sqrt(gridItems.Count)))
                 {
-                    GridItem target = gridItems.Find(i => i.gridPosition == neighborPosition);
+                    GridItem neighborItem = gridItems.Find(i => i.gridPosition == neighborPosition);
 
-                    if (target != null && (!usedItems.Contains(target) || usedItems.Count <= targetGroupSize) &&
-                        !visitedLocal.Contains(target))
+                    if (neighborItem != null && !usedItems.Contains(neighborItem) &&
+                        !visitedLocal.Contains(neighborItem))
                     {
-                        if (!WillCreateEmptySpaceInternal(target, gridItems, usedItems, visitedLocal, targetGroupSize))
+                        if (!WillCreateEmptySpaceInternal(neighborItem, gridItems, usedItems, visitedLocal,
+                                targetGroupSize))
                         {
-                            c++;
+                            approvedDirections++;
                         }
                     }
-                    else if (visitedLocal.Contains(target) || (usedItems.Contains(target) || usedItems.Count <= targetGroupSize))
+                    else if (visitedLocal.Contains(neighborItem) || usedItems.Contains(neighborItem))
                     {
-                        c++;
+                        approvedDirections++;
                     }
                 }
                 else
                 {
-                    c++;
+                    approvedDirections++;
                 }
             }
 
-            return c < _directions.Length;
+            return approvedDirections < _directions.Length;
         }
 
         private bool WillCreateEmptySpaceInternal(GridItem neighborItem, List<GridItem> gridItems,
@@ -160,9 +176,7 @@ namespace App.Scripts.Generator.Handlers
             stack.Push(neighborItem);
 
             int island = 1;
-
-            HashSet<GridItem> visitedLocal = new HashSet<GridItem> { neighborItem };
-            visitedLocal.AddRange(visited);
+            visited.AddRange(visited);
 
             while (stack.Count > 0)
             {
@@ -177,24 +191,25 @@ namespace App.Scripts.Generator.Handlers
                     {
                         GridItem currentItem = gridItems.Find(i => i.gridPosition == neighborPosition);
 
-                        if (currentItem != null && !visitedLocal.Contains(currentItem) &&
-                            (!usedItems.Contains(currentItem) || usedItems.Count <= targetGroupSize))
+                        if (currentItem != null && !visited.Contains(currentItem) &&
+                            (!usedItems.Contains(currentItem)))
                         {
                             stack.Push(currentItem);
-                            visitedLocal.Add(currentItem);
+                            visited.Add(currentItem);
                             island++;
                         }
                     }
                 }
             }
 
-            if (island / targetGroupSize > 1)
+            if (island / targetGroupSize >= (_targetUseItems - (usedItems.Count + island)) / targetGroupSize)
             {
                 return false;
             }
 
-            if (island % targetGroupSize < _targetGroupsCount)
+            if (island % targetGroupSize == 0)
             {
+                return false;
             }
 
             return true;
